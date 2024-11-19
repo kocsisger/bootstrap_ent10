bc2cb0475732392eabc9ac7e05100d369cc51e68
John Smith <you@inf.unideb.hu>
2024-11-19 10:42:35 +0100

    test added

--- src/test/java/hu/unideb/inf/bootstrap_en/BootstrapEnApplicationTests.java
+++ src/test/java/hu/unideb/inf/bootstrap_en/BootstrapEnApplicationTests.java
@@ -1,13 +1,28 @@
 package hu.unideb.inf.bootstrap_en;
 
+import hu.unideb.inf.bootstrap_en.model.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace =
+		AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+
 class BootstrapEnApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Person p = new Person(10,
+				"John",
+				"Taylor",
+				"jt@example.com",
+				"apple123",
+				true);
+
 	}
 
 }
