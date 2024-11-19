4b090894e50d4859aa9226b84f47d895b10f4d25
John Smith <you@inf.unideb.hu>
2024-11-19 10:27:20 +0100

    Person entity added

--- src/main/java/hu/unideb/inf/bootstrap_en/model/Person.java
+++ src/main/java/hu/unideb/inf/bootstrap_en/model/Person.java
@@ -1,6 +1,15 @@
 package hu.unideb.inf.bootstrap_en.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue
     private long id;
     private String firstName;
     private String lastName;
