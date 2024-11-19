f8463b992a59281f168389af10c7aea670b55b22
John Smith <you@inf.unideb.hu>
2024-11-19 11:00:27 +0100

    Hello world

--- src/test/java/hu/unideb/inf/bootstrap_en/BootstrapEnApplicationTests.java
+++ src/test/java/hu/unideb/inf/bootstrap_en/BootstrapEnApplicationTests.java
@@ -1,7 +1,9 @@
 package hu.unideb.inf.bootstrap_en;
 
 import hu.unideb.inf.bootstrap_en.model.Person;
+import hu.unideb.inf.bootstrap_en.model.PersonRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,15 +16,19 @@ import org.springframework.test.annotation.Rollback;
 
 class BootstrapEnApplicationTests {
 
-	@Test
+	@Autowired
+	PersonRepository personRepository;
+
+	//@Test
 	void contextLoads() {
-		Person p = new Person(10,
+		Person p = new Person(100,
 				"John",
 				"Taylor",
 				"jt@example.com",
 				"apple123",
 				true);
 
+		personRepository.save(p);
 	}
 
 }
