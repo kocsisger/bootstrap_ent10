529207d89482b79a6a15e748a434a825f17a5446
John Smith <you@inf.unideb.hu>
2024-11-19 10:20:16 +0100

    Initial commit

---
+++ src/main/java/hu/unideb/inf/bootstrap_en/BootstrapEnApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.bootstrap_en;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapEnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapEnApplication.class, args);
+	}
+
+}
