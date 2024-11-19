121ace9867b5c2f846cdd72d3599a2566b4e64bf
John Smith <you@inf.unideb.hu>
2024-11-19 10:25:19 +0100

    Perrson class added

---
+++ src/main/java/hu/unideb/inf/bootstrap_en/model/Person.java
@@ -0,0 +1,10 @@
+package hu.unideb.inf.bootstrap_en.model;
+
+public class Person {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private boolean enabled;
+}
